2b323c3b3ede10c2527a2e274490699a33f9bc2e
Nick <nbarrameda89@gmail.com>
2021-11-29 15:17:57 -0500

    object added

--- dist/main.js
+++ dist/main.js
@@ -25,7 +25,17 @@ eval("function Asteroid () {\r\n    console.log(\"this is the asteroid.js file!\
   \**********************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("const Asteroid = __webpack_require__(/*! ./asteroid.js */ \"./src/asteroid.js\");\r\n\r\nwindow.asteroid = Asteroid;\r\n\r\nAsteroid();\n\n//# sourceURL=webpack://W9D1/./src/index.js?");
+eval("const Asteroid = __webpack_require__(/*! ./asteroid.js */ \"./src/asteroid.js\");\r\nconst MovingObject = __webpack_require__(/*! ./moving_object.js */ \"./src/moving_object.js\");\r\nwindow.asteroid = Asteroid;\r\nwindow.MovingObject = MovingObject;\r\n\r\nAsteroid();\r\n\r\ndocument.addEventListener('DOMContentLoaded', () => {\r\n    const canvas1 = document.getElementById('canvas');\r\n    const ctx = canvas1.getContext('2d');\r\n    canvas1.width = 900;\r\n    canvas1.height = 600;\r\n    \r\n    const circle = new MovingObject([100, 100], [1, 1], 50, \"red\");\r\n    circle.draw(ctx); \r\n})\r\n\n\n//# sourceURL=webpack://W9D1/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/moving_object.js":
+/*!******************************!*\
+  !*** ./src/moving_object.js ***!
+  \******************************/
+/***/ ((module) => {
+
+eval("function MovingObject (pos, vel, radius, color) {\r\n    this.pos = pos;\r\n    this.vel = vel;\r\n    this.radius = radius;\r\n    this.color = color;\r\n}\r\n\r\nMovingObject.prototype.draw = function (ctx) {\r\n    ctx.beginPath();\r\n    ctx.arc(this.pos[0], this.pos[1], this.radius, 0, 2*Math.PI, true);\r\n    ctx.closePath;\r\n    ctx.fillStyle = this.color;\r\n    ctx.fill();\r\n}\r\n\r\n\r\nmodule.exports = MovingObject;\n\n//# sourceURL=webpack://W9D1/./src/moving_object.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const Asteroid = require('./asteroid.js');
-
+const MovingObject = require('./moving_object.js');
 window.asteroid = Asteroid;
+window.MovingObject = MovingObject;
 
 Asteroid();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas1 = document.getElementById('canvas');
+    const ctx = canvas1.getContext('2d');
+    canvas1.width = 900;
+    canvas1.height = 600;
+    
+    const circle = new MovingObject([100, 100], [1, 1], 50, "red");
+    circle.draw(ctx); 
+})
